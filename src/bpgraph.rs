a9a5acd88d1efdbe4f3f76d67feda5f26729db79
Vijaya Anand <sunrockers8@gmail.com>
2023-08-23 13:00:57 +0530

    abp-test 14

--- src/bpgraph.rs
+++ src/bpgraph.rs
@@ -195,17 +195,14 @@ where
                             let mut r_log: MsgT = r.get_log();
                             r_log.normalize();
                             let mut q_log = log_prob_curr.subtract(r_log);
-                            println!("Before exponent Distn : {:?}", q_log);
                             let mut q = q_log.exponent();
                             q.normalize();
-                            println!("After exponent Distn : {:?}", q);
                             var_node.update_push(check_node_index, q);
                         }
                         else {
                             println!("Before exponent Distn : {:?}", log_prob_curr);
                             let mut q = log_prob_curr.exponent();
                             q.normalize();
-                            println!("After exponent Distn : {:?}", q);
                             var_node.update_push(check_node_index, q);
                         }
                     };
